87724bb7a0112fca77d97fffe61806715d9811fa
Lucas Santos <leos9877@gmail.com>
2022-03-09 18:26:01 -0300

    :pushpin: modify archives.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
 import Router from "./routes/intex";
+import swaggerUi from "swagger-ui-express";
 
 const PORT = process.env.PORT || 8000;
 
@@ -9,6 +10,18 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
+
+app.use(Router);
+
+app.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  })
+);
 
 app.use(Router);
 
